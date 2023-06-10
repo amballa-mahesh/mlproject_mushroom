dd4bfeb898033ff62932f96146895ecd14d6f465
mahesh-amballa <amballa.mahesh89@gmail.com>
2023-06-10 13:00:50 +0530

    final model commit

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 
 try:
-    data = 'artifacts/train.csv'
+    data = 'artifacts/raw.csv'
     logging.info('Data Loaded')
     x_trans,y_trans = data_cleaning(data)
     x_trans.to_csv(os.path.join('artifacts','x_trans.csv'), index =False, header =True)
